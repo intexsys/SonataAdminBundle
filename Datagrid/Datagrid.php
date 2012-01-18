4cd071067688caf496f6e52abf2649107755f2d7
Andrey Suslin <andrey.suslin@intechsystems.lv>
2012-01-18 13:25:59 +0200

    Added  dynamic per_page setting and its processing

--- Datagrid/Datagrid.php
+++ Datagrid/Datagrid.php
@@ -102,6 +102,7 @@ class Datagrid implements DatagridInterface
         $this->formBuilder->add('_sort_by', 'hidden');
         $this->formBuilder->add('_sort_order', 'hidden');
         $this->formBuilder->add('_page', 'hidden');
+        $this->formBuilder->add('_per_page', 'hidden');
 
         $this->form = $this->formBuilder->getForm();
         $this->form->bind($this->values);
@@ -109,6 +110,7 @@ class Datagrid implements DatagridInterface
         $this->query->setSortBy(isset($this->values['_sort_by']) ? $this->values['_sort_by'] : null);
         $this->query->setSortOrder(isset($this->values['_sort_order']) ? $this->values['_sort_order'] : null);
 
+        $this->pager->setMaxPerPage(isset($this->values['_per_page']) ? $this->values['_per_page'] : 10);
         $this->pager->setPage(isset($this->values['_page']) ? $this->values['_page'] : 1);
         $this->pager->setQuery($this->query);
         $this->pager->init();
